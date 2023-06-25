00d9b24df94cd36ebfd0195e3bf572a84e41abce
YousefElshabrawy <yousefatefelshabrawy@gmail.com>
2023-06-25 23:27:50 +0300

    Add the SMT class

--- Integration/SMT/smt.py
+++ Integration/SMT/smt.py
@@ -1,44 +1,50 @@
 from utils import *
-# Import the decoders
 import ibm1 as ibm1
 import phrase_based as phrase_based
 
-# Load the language models 
-bigram_lm_forward = load_model('models/lm/bigram_lm_forward.pkl')
-bigram_lm_backward = load_model('models/lm/bigram_lm_backward.pkl')
 
-# Load the translation models
-ibm1_forward = load_model('models/tm/ibm1_forward.pkl')
-ibm1_backward = load_model('models/tm/ibm1_backward.pkl')
-phrase_based_forward = load_model('models/tm/phrase_based_forward.pkl')
-phrase_based_backward = load_model('models/tm/phrase_based_backward.pkl')
+class SMT:
+    def __init__(self):
+        # Load the language models 
+        self.bigram_lm_forward = load_model('models/lm/bigram_lm_forward.pkl')
+        self.bigram_lm_backward = load_model('models/lm/bigram_lm_backward.pkl')
 
-# Instantiate the decoders
-ibm1_forward_translator = ibm1.Decoder(ibm1_forward, bigram_lm_forward)
-ibm1_backward_translator = ibm1.Decoder(ibm1_backward, bigram_lm_backward)
-phrase_based_forward_translator = phrase_based.Decoder(phrase_based_forward, bigram_lm_forward)
-phrase_based_backward_translator = phrase_based.Decoder(phrase_based_backward, bigram_lm_backward)
+        # Load the translation models
+        self.ibm1_forward = load_model('models/tm/ibm1_forward.pkl')
+        self.ibm1_backward = load_model('models/tm/ibm1_backward.pkl')
+        self.phrase_based_forward = load_model('models/tm/phrase_based_forward.pkl')
+        self.phrase_based_backward = load_model('models/tm/phrase_based_backward.pkl')
 
-def forward_translate(f):
-    e = phrase_based_forward_translator.translate(f)
-    if  not e : # If the phrase based decoder fails to translate, use the IBM1 decoder
-        e = ibm1_forward_translator.translate(f)
-    return e
+        # Instantiate the decoders
+        self.ibm1_forward_translator = ibm1.Decoder(self.ibm1_forward, self.bigram_lm_forward)
+        self.ibm1_backward_translator = ibm1.Decoder(self.ibm1_backward, self.bigram_lm_backward)
+        self.phrase_based_forward_translator = phrase_based.Decoder(self.phrase_based_forward, self.bigram_lm_forward)
+        self.phrase_based_backward_translator = phrase_based.Decoder(self.phrase_based_backward, self.bigram_lm_backward)
 
-def backward_translate(f):
-    e = phrase_based_backward_translator.translate(f)
-    if  not e : # If the phrase based decoder fails to translate, use the IBM1 decoder
-        e = ibm1_backward_translator.translate(f)
-    return e
+    # Translate from English to ASL Gloss
+    def forward_translate(self, f):
+        e = self.phrase_based_forward_translator.translate(f)
+        if  not e : # If the phrase based decoder fails to translate, use the IBM1 decoder
+            e = self.ibm1_forward_translator.translate(f)
+        return e
+    
+    # Translate from ASL Gloss to English
+    def backward_translate(self, f):
+        e = self.phrase_based_backward_translator.translate(f)
+        if  not e : # If the phrase based decoder fails to translate, use the IBM1 decoder
+            e = self.ibm1_backward_translator.translate(f)
+        return e
 
-aslg = forward_translate('the girl is in france')
+# Test the SMT
+smt = SMT()
+aslg = smt.forward_translate('the girl is in france')
 print(aslg)
 
-en = backward_translate(aslg)
+en = smt.backward_translate(aslg)
 print(en)
 
-aslg = forward_translate('the girl in france')
+aslg = smt.forward_translate('the girl in france')
 print(aslg)
 
-en = backward_translate(aslg)
+en = smt.backward_translate(aslg)
 print(en)
